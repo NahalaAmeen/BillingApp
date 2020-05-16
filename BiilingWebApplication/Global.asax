013870d450798240ae2f6a0582919a0712b97fb2
Neethu <Neethu@LAPTOP-JM6IDTV3>
2020-05-16 22:19:04 +0530

    Add project files.

---
+++ BiilingWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BiilingWebApplication.MvcApplication" Language="C#" %>
---
+++ BiilingWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BiilingWebApplication.Startup))]
+namespace BiilingWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
